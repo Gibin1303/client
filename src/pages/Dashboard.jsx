7678e058e80b3f8fde14af457dc581d6a649245d
Gibin <gibin@aventusinformatics.com>
2026-04-27 23:51:02 +0530

    Dahboard ui completed

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { dummyAdminDashboardData, dummyEmployeeDashboardData } from "../assets/assets";
+import Loading from "../components/Loading";
+import EmployeeDashboard from "../components/EmployeeDashboard";
+import AdminDahboard from "../components/AdminDahboard";
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-export default Dashboard
+  useEffect(() => {
+    setData(dummyEmployeeDashboardData);
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
+  if (loading) return <Loading/>;
+  if (!data)
+    return (
+      <p className="text-center text-slate-500 py-12">
+        Failed to load Dashboard
+      </p>
+    );
+
+  if (data.role === "ADMIN") {
+    return <AdminDahboard data={data}/>
+  } else {
+    return <EmployeeDashboard data={data}/>;
+  }
+};
+
+export default Dashboard;
